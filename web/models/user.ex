8c46536e4286aaf54f17f58ad25e54df81e3ae12
Chad Ostrowski <hi@chadoh.com>
2018-05-05 18:27:42 -0400

    Bugfix: authentication
    Three errors were preventing successful authentication:
    
    * The layout file was never rendering child views, so it only ever
      showed the login form. This broke everything!
    * The `User.changeset` default was set to `:empty`, which is what the
      Programming Phoenix book says to do, but as we covered in class is
      outdated. The new standard is to use an empty Map, `%{}`
    * The New User form had no submit button, which prevented the form from
      being submitted!

--- web/models/user.ex
+++ web/models/user.ex
@@ -8,7 +8,7 @@ defmodule ComicApp.User do
     timestamps
   end
 
-  def changeset(model, params \\ :empty) do
+  def changeset(model, params \\ %{}) do
 model
 |> cast(params, ~w(name username), [])
 |> validate_length(:username, min: 1, max: 20)
